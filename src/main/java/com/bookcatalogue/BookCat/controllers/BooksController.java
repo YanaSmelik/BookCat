1d2398b89e9b775910198475f5bfc57e8f141f15
Yana Smelyk <smelikyana@gmail.com>
2021-02-26 17:38:40 +0000

    changed @Controller to @RestController annotation in BooksController class

--- src/main/java/com/bookcatalogue/BookCat/controllers/BooksController.java
+++ src/main/java/com/bookcatalogue/BookCat/controllers/BooksController.java
@@ -4,12 +4,9 @@ import com.bookcatalogue.BookCat.domain.BookDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 //@RequestMapping //("/books")
 public class BooksController {
 
